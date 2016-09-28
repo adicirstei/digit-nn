5aca907ae7d36daec7acee4dafbfa78882670d7d
Adrian <adicirstei@gmail.com>
2016-09-28 13:10:12 +0300

    implemented some network

--- network.fsx
+++ network.fsx
@@ -1,8 +1,32 @@
-#I "packages/MathNet.Numerics/lib/net40"
-#I "packages/MathNet.Numerics.FSharp/lib/net40"
+#load "packages/MathNet.Numerics.FSharp/MathNet.Numerics.fsx"
 
-#r "MathNet.Numerics.Fsharp.dll"
-#r "MathNet.Numerics.dll"
+open MathNet.Numerics.LinearAlgebra
+//open MathNet.Numerics.LinearAlgebra.Double
+
+type Net = {
+  numLayers : int
+  sizes : int list
+  biases : (Matrix<float>) list
+  weights : (Matrix<float>) list
+}
+
+let network sizes =
+  {
+    numLayers = List.length sizes
+    sizes = sizes
+    biases = [for y in sizes.[1..] -> DenseMatrix.randomStandard<float>y 1]
+    weights = [for x, y in List.zip sizes.[1..] (List.take (sizes.Length - 1) sizes)  -> 
+                DenseMatrix.randomStandard<float> y x ]
+  }
 
 
-MathNet.Numerics.LinearAlgebra.MatrixExtensions.matrix 
+let sigmoidE (z:float) : float = 1.0 / (1.0 + exp (-z))
+let sigmoid : (Matrix<float> -> Matrix<float>) = Matrix.map sigmoidE
+
+let feedforward net a =
+  List.zip net.biases net.weights 
+  |> List.fold (fun s (b,w) -> sigmoid ((w * s) + b) ) a
+
+let net = network [2;3;1]
+
+feedforward net (DenseMatrix.randomStandard<float> 1 2)
