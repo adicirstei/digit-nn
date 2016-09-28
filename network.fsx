3ed55ca13400608d420d4f0688de13dfbbde5cbf
Adrian <adicirstei@gmail.com>
2016-09-28 10:40:13 +0300

    initial commit

---
+++ network.fsx
@@ -0,0 +1,8 @@
+#I "packages/MathNet.Numerics/lib/net40"
+#I "packages/MathNet.Numerics.FSharp/lib/net40"
+
+#r "MathNet.Numerics.Fsharp.dll"
+#r "MathNet.Numerics.dll"
+
+
+MathNet.Numerics.LinearAlgebra.MatrixExtensions.matrix 
