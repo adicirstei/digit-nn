9ed7e695b2a68dcfbc670d02bcb17755fe819247
Adrian <adicirstei@gmail.com>
2016-10-03 14:34:26 +0300

    added some status messages

--- train.fsx
+++ train.fsx
@@ -4,7 +4,7 @@
 #load "mnist.fsx"
 
 open MathNet.Numerics.LinearAlgebra
-
+printfn "loading MNIST data..."
 let data =  Mnist.getData()
 
 let trd, vld, tsd = data
@@ -23,8 +23,8 @@ let testData:Network.TestData =
   tsd
   |> Array.map (fun (d, l) -> (DenseMatrix.ofColumnArrays [| Array.map (fun px ->  (float px) ) d |], l) )
 
-let net = Network.network [784;56;10]
-
-let trainedNet = Network.SGD net trainingData 50 10 0.5 testData
-
-Persistence.saveNet trainedNet "nets/trained-56N-50E.net"
+let net = Network.network [784;44;10]
+printfn "Started training network"
+let trainedNet = Network.SGD net trainingData 50 10 2.5 testData
+printfn "Saving trained network"
+Persistence.saveNet trainedNet "nets/trained-44N-50E.net"
