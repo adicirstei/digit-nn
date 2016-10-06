828eb8cb46aa66f9226445b20b7b0d87a7d53d00
Adrian <adicirstei@gmail.com>
2016-10-06 13:18:47 +0300

    changed the chars for rendering

--- render.fsx
+++ render.fsx
@@ -1,7 +1,9 @@
 let renderImage (img:byte []) : string =
-  let grad = " .:oO"
+  //let grad = " -·♦■█"
+  let grad = " .,:;xX&@"
+  let l = float (String.length grad - 1)
   img
-  |> Array.map (fun b -> string grad.[int ( (float b / 255.0) * 4.0) ])
+  |> Array.map (fun b -> string grad.[int ( (float b / 255.0) * l) ])
   |> Array.chunkBySize 28
   |> Array.map (fun s -> String.concat "" s)
   |> String.concat "\n"
